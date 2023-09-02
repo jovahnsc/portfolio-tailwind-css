fa2305c444b9b29db7b846e7c74554bac42af3e4
jovahn <jovanshann@gmail.com>
2023-09-02 16:48:01 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#06b6d4',
+        dark: '#0f172a',
+      }
+    },
+  },
+  plugins: [],
+}
+
