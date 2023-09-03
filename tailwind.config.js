5ae3fe1de20683b09d8a3dd15fd9eeaa2507837e
jovahnsc <jovanshann@gmail.com>
2023-09-03 22:50:15 +0700

    Menambahkan About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
-        primary: '#06b6d4',
-        dark: '#0f172a',
-      }
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
